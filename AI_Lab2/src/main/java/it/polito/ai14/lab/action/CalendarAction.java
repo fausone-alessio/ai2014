1277cc7052c19ccd2da3b80b62a11a672d9861db
dash991 <bianco.riccardo@gmail.com>
2014-03-31 17:32:00 +0200

    Il valore String della mappa è l'utente che ha prenotato la sala XX alle ore XX:00 del giorno XX/XX/XXX.

--- AI_Lab2/src/main/java/it/polito/ai14/lab/action/CalendarAction.java
+++ AI_Lab2/src/main/java/it/polito/ai14/lab/action/CalendarAction.java
@@ -1,15 +1,20 @@
 package it.polito.ai14.lab.action;
 
+import it.polito.ai14.lab.Sala;
+
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
+
 import org.apache.struts2.ServletActionContext;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class CalendarAction extends ActionSupport {
+public class CalendarAction extends ActionSupport  {
 	
 	private static final long serialVersionUID = 2777557863534616656L;
 	private Integer year;
@@ -17,17 +22,26 @@ public class CalendarAction extends ActionSupport {
 	private Integer room;
 	
 	public String execute() {
-
+		ServletContext servletContext = ServletActionContext.getServletContext();  
+		Sala[] palazzo = (Sala[]) servletContext.getAttribute("palazzo"); 
+		
 		Calendar calendario = new GregorianCalendar();
+		calendario.clear();
 		calendario.set(Calendar.YEAR, year);
 		calendario.set(Calendar.WEEK_OF_YEAR, week);
 		calendario.set(Calendar.HOUR_OF_DAY, 8);
-		calendario.set(Calendar.MINUTE, 00);
 		
+		String prenotante = "";
 		Map <Date, String> slots = new LinkedHashMap<Date, String>();
 		for (int ora = 0; ora < 15; ora++) {
 			for (int giorno = 0; giorno < 7; giorno++) {
-				slots.put(calendario.getTime(), "Lorem ipsum");
+				
+				if (palazzo[room].getPrenotazioni().containsKey(calendario.getTime()))
+					prenotante = palazzo[room].getPrenotazioni().get(calendario.getTime());
+				else
+					prenotante = "";
+				
+				slots.put(calendario.getTime(), prenotante);
 				calendario.add(Calendar.DAY_OF_YEAR, 1);
 			}
 			calendario.add(Calendar.HOUR_OF_DAY, 1);
