3f59f6c14b516c087834d01cbc645181ee7bc341
dash991 <bianco.riccardo@gmail.com>
2014-03-31 17:33:43 +0200

    Aggiunta una prenotazione per fare un po' di test senza dover inserirla ogni volta all'atto del deployment (anche perchè ancora non possiamo prenotare un bel cazzo, LOL)

--- AI_Lab2/src/main/java/it/polito/ai14/lab/ApplicationManager.java
+++ AI_Lab2/src/main/java/it/polito/ai14/lab/ApplicationManager.java
@@ -1,12 +1,16 @@
 package it.polito.ai14.lab;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.ConcurrentHashMap;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class ApplicationManager implements ServletContextListener {
 
     public void contextInitialized(ServletContextEvent sce) {
-        // per accederci: get application context get attribute modello
     	Sala[] palazzo = new Sala[4];
     	palazzo[0] = new Sala();
     	palazzo[0].setNome("S1");
@@ -17,6 +21,19 @@ public class ApplicationManager implements ServletContextListener {
     	palazzo[3] = new Sala();
     	palazzo[3].setNome("S4");
     	
+    	// Ho messo una prenotazione di Alessio in sala 4 alle 11 del 2/04/2014
+    	// per fare un po' di prove.
+    	ConcurrentHashMap <Date, String> p = palazzo[3].getPrenotazioni();
+		Calendar calendario = new GregorianCalendar();
+		calendario.clear();
+		calendario.set(Calendar.YEAR, 2014);
+		calendario.set(Calendar.MONTH, Calendar.APRIL);
+		calendario.set(Calendar.DAY_OF_MONTH, 2);
+		calendario.set(Calendar.HOUR_OF_DAY, 11);
+		calendario.set(Calendar.MINUTE, 00);
+		p.put(calendario.getTime(), "alessio");
+		palazzo[3].setPrenotazioni(p);
+		
         sce.getServletContext().setAttribute("palazzo", palazzo);
     }
 
