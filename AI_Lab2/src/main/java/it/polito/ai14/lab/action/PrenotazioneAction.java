76767353d6e1902b20bd8909c5dd90d41745950d
dash991 <bianco.riccardo@gmail.com>
2014-04-05 12:30:09 +0200

    Scheletro migliorato della prenotazione. Ancora non definitivo.

--- AI_Lab2/src/main/java/it/polito/ai14/lab/action/PrenotazioneAction.java
+++ AI_Lab2/src/main/java/it/polito/ai14/lab/action/PrenotazioneAction.java
@@ -1,6 +1,18 @@
 package it.polito.ai14.lab.action;
 
+import it.polito.ai14.lab.Sala;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.servlet.ServletContext;
+
+import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
@@ -13,10 +25,61 @@ public class PrenotazioneAction extends ActionSupport {
 	private String startTime;
 	
 	public String execute() {
+		ServletContext servletContext = ServletActionContext.getServletContext();  
     	Map<String, Object> session = ActionContext.getContext().getSession();
+    	
     	String utente = (String) session.get("username");
-		System.out.println(utente + " " + day + " " + month + " " + year + " " + startTime + " " + room);
+    	Sala[] palazzo = (Sala[]) servletContext.getAttribute("palazzo");
+    	int r = Integer.parseInt(room);
+    	
+		Calendar calendario = new GregorianCalendar();
+		calendario.clear();
+		calendario.set(Calendar.YEAR, Integer.parseInt(year));
+		calendario.set(Calendar.MONTH, Integer.parseInt(month) - 1);
+		calendario.set(Calendar.DAY_OF_MONTH, Integer.parseInt(day));
+		calendario.set(Calendar.HOUR_OF_DAY, Integer.parseInt(startTime));
+		
+		ConcurrentHashMap <Date, String> prenotazioni = palazzo[r].getPrenotazioni();
+    	prenotazioni.putIfAbsent(calendario.getTime(), utente);
 		return SUCCESS;
+	}
+	
+	public void validate() {
+		// TODO Cazziare prenotazioni in slot pieni
+		ServletContext servletContext = ServletActionContext.getServletContext();  
+    	Map<String, Object> session = ActionContext.getContext().getSession();
+    	
+    	String utente = (String) session.get("username");
+    	Sala[] palazzo = (Sala[]) servletContext.getAttribute("palazzo");
+    	Date oggi = Calendar.getInstance().getTime();
+    	
+    	int nPrenotazioni = 0;
+    	for (Sala s: palazzo) {
+    		ConcurrentHashMap <Date, String> prenotazioni = s.getPrenotazioni();
+    		Set <Map.Entry<Date, String>> log = prenotazioni.entrySet();
+    		Iterator <Map.Entry<Date, String>> it = log.iterator();
+    		while (it.hasNext()) {
+    			Map.Entry<Date, String> riga = it.next();
+    			Date tempo = riga.getKey(); 
+    			String prenotante = riga.getValue();
+    			
+    			// TODO Sbagliato, devo guardarlo insieme alle prenotazioni, il cazziamento del passato si può fare fuori
+    			if (tempo.compareTo(oggi) < 0) {
+    				System.out.println("WHADDUP!");
+    				addActionError("Non puoi effettuare una prenotazione nel passato.");
+    				return;
+    			}
+    			else {
+    				// TODO Cazziare prenotazioni nel passato
+    				if (prenotante.equals(utente))
+    					nPrenotazioni++;
+    				if (nPrenotazioni >= 4) {
+        				addActionError("Hai più di quattro prenotazioni pendenti");
+        				return;
+    				}
+    			}
+    		}
+    	}
 	}
 
 	public String getDay() {
