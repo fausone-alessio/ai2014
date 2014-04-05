3a794f96c8e094a87a666fc4e8ea77b1afb56c9f
alessio <a.fausone@zero11.it>
2014-04-05 16:19:48 +0200

    Aggiunto security interceptor che controlla se l'utente è loggato per effettuare tutte le azioni tranne la consultazione

---
+++ AI_Lab2/src/main/java/it/polito/ai14/lab/interceptor/SecurityInterceptor.java
@@ -0,0 +1,35 @@
+package it.polito.ai14.lab.interceptor;
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+public class SecurityInterceptor implements Interceptor {
+	private static final long serialVersionUID = 5367228552868778914L;
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String intercept(ActionInvocation invocation) throws Exception {
+    	Map<String, Object> session = ActionContext.getContext().getSession();
+    	
+    	String utente = (String) session.get("username");
+
+	    if (utente == null) {
+	        return ActionSupport.LOGIN;
+	    } else {
+	        return invocation.invoke ();
+	    }
+	}
+
+}
